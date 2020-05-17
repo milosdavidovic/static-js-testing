4e6fbd552cd9564a695f02d9418f84d18c23d6d9
Milos Davidovic <milos.davidovic@embracesbs.com>
2020-05-17 16:18:40 +0200

    example added

---
+++ src/example.js
@@ -0,0 +1,8 @@
+const greeter = (firstName,surName) => {
+  console.log("Hello " + firstName + " " + lastName);
+}
+
+const firstName = "John"
+const lastName = 'Smith';
+
+greeter (firstName, lastName);
