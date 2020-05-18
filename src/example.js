006d1091f0a9249977a6fb94741f803d66c7a064
Milos Davidovic <milos.davidovic@embracesbs.com>
2020-05-18 08:36:40 +0200

    configured prettier

--- src/example.js
+++ src/example.js
@@ -1,9 +1,9 @@
-const greeter = (firstName,surName) => {
+const greeter = (firstName, lastName) => {
   // eslint-disable-next-line no-console
-  console.log("Hello " + firstName + " " + lastName);
-}
+  console.log(`Hello ${firstName} ${lastName}`);
+};
 
-var firstName = "John"
-const lastName = 'Smith';
+var firstName = "John";
+const lastName = "Smith";
 
-greeter (firstName, lastName);
+greeter(firstName, lastName);
